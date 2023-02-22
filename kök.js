08ac751bc4ff4ba85ee17fc49b93460f60a9b086
Quarps <106306249+Quarps@users.noreply.github.com>
2023-02-22 12:49:29 +0100

    kitchen, created a scenary

---
+++ kök.js
@@ -0,0 +1,14 @@
+function kitchen() {
+  background(255, 255, 255);
+  noStroke();
+  fill(184, 139, 74);
+  rect(0, 0, width, height / 2);
+
+  fill(231, 236, 239);
+  rect(0, height / 2, width, height / 2);
+
+  fill(0, 0, 0);
+  rect(0, height / 2, width, 52);
+}
+
+kitchen();
